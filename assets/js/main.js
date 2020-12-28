ba2cd958d23e37554d92cd4ef6e94abdc4d2f6ec
bakarfreelancer <abubakarfreelancer96@gmail.com>
2020-12-28 22:44:30 +0500

    added color changer to all pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,11 @@ colorsDiv       = document.querySelector('.colors');
 styleTag        = document.querySelector('style');
 function openClose(){
     if(openCloseButton.classList.contains('closed')){
-        openCloseButton.innerHTML = '<i class="fas fa-eye"></i>';
+        openCloseButton.innerHTML = '<i class="fas fa-times"></i>';
         openCloseButton.classList.remove('closed');
         colorsDiv.classList.remove('hide');
     }else{
-        openCloseButton.innerHTML = '<i class="fas fa-times"></i>';
+        openCloseButton.innerHTML = '<i class="fas fa-eye"></i>';
         openCloseButton.classList.add('closed');
         colorsDiv.classList.add('hide');
     }
